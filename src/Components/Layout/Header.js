946801829aa401c5aebfea6ce0cb260358b13254
Ana Castedo <anaccastedo@gmail.com>
2023-02-13 14:21:24 +0000

    Header of App - button with icons, name and nuimber of items

---
+++ src/Components/Layout/Header.js
@@ -0,0 +1,18 @@
+import {Fragment} from "react";
+import mealsImage from '../../assets/meals_off.jpg'
+import classes from './Header.module.css'
+import HeaderCartButton from "./HeaderCartButton";
+
+const Header = props =>{
+return <Fragment>
+    <header className={classes.header}>
+        <h1>ReactMeals</h1>
+        <HeaderCartButton />
+    </header>
+    <div className={classes['main-image']}>
+        <img src={mealsImage} alt='Table full of delicious food'/>
+    </div>
+</Fragment>
+}
+
+export default Header;
