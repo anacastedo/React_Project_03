66884c5212826e7cce580fd74a615fbebb7e2144
Ana Castedo <anaccastedo@gmail.com>
2023-02-20 19:55:57 +0000

    Order Food App Complete

--- src/Components/Layout/HeaderCartButton.js
+++ src/Components/Layout/HeaderCartButton.js
@@ -1,13 +1,31 @@
 import classes from './HeaderCartButton.module.css';
 import CartIcon from '../Cart/CartIcon'
-import {useContext} from "react";
+import {useContext, useEffect, useState} from "react";
 import CartContext from "../../store/cart-context";
 const HeaderCartButton =(props)=>{
 
+    const [btnIsHighlighted, setBtnIsHighlighted]=useState(false);
     const cartContext=useContext(CartContext)
+    const {items} =cartContext
     const numberCartItems=cartContext.items.reduce((curNumber, item)=>{return curNumber+item.amount},0)
 
-    return <button className={classes.button} onClick={props.onClick}>
+    const btnClasses = `${classes.button} ${btnIsHighlighted?classes.bump:''}`
+
+    useEffect(()=>{
+        if(cartContext.items.length ===0){
+            return
+        }
+        setBtnIsHighlighted(true);
+
+        const timer= setTimeout (()=>{
+            setBtnIsHighlighted(false)
+        }, 300)
+
+        return ()=>{
+            clearTimeout(timer)
+        }
+    }, [items])
+    return <button className={btnClasses} onClick={props.onClick}>
     <span className={classes.icon}>
         <CartIcon />
     </span>
