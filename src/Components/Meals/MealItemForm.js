ac91b82170d9394e1362131b56c4e7ce529dab24
Ana Castedo <anaccastedo@gmail.com>
2023-02-20 17:45:38 +0000

    Using bind to pass arguments to handler functions, cart items refactored

--- src/Components/Meals/MealItemForm.js
+++ src/Components/Meals/MealItemForm.js
@@ -14,34 +14,31 @@ const MealItemForm = props =>{
 
         if(enteredAmount.trim().length===0 || enteredAmountNumber<1 ||enteredAmountNumber>5){
             setAmountIsValid(false)
-
             return
         }
+
         setAmountIsValid(true)
         props.onAddToCart(enteredAmountNumber)
-
-
-
-
     }
 
 
-    return <form className={classes.form} onSubmit={submitHandler}>
-        <Input
-            ref={amountInputRef}
-            label='Amount'
-            input={{
-                id:'amount_'+props.id,
-                type:'number',
-                min:'1',
-                max: '5',
-                step:'1',
-                defaultValue:'1'
-            }}
-        />
-        <button>+ Add</button>
-        {!amountIsValid && <p>Please enter a valid amount (1-5)</p>}
-    </form>
+    return(
+        <form className={classes.form} onSubmit={submitHandler}>
+            <Input
+                ref={amountInputRef}
+                label='Amount'
+                input={{
+                    id:'amount_'+props.id,
+                    type:'number',
+                    min:'1',
+                    max: '5',
+                    step:'1',
+                    defaultValue:'1'
+                }}
+            />
+            <button>+ Add</button>
+            {!amountIsValid && <p>Please enter a valid amount (1-5)</p>}
+        </form>)
 }
 
 export default MealItemForm
